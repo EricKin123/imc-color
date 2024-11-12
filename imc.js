bc5a560a34210afbe2a7b939858a491146f35b3f
Eric <163418429+EricKin123@users.noreply.github.com>
2024-11-12 12:43:32 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,23 @@
+let altura = parseFloat( prompt("Escreva sua altura: "));
+let peso = parseFloat( prompt("Escreva sua peso: "));
+let imc = (altura/(altura*peso));
+
+document.write("O imc é igual a:  ")
+
+if(isNaN(peso) || isNaN(altura) || peso <=0 || altura<=0){
+        document.write("Digite valores validos: ");
+    } else{
+        let imc = (peso/(altura*altura)).toFixed(2);
+        document.write(" : <span style= 'color:red;'>",imc, "</span>");
+document.write("<br>")
+        if(imc<=18){
+            document.write(" Seu imc está abaixo do normal!! <");
+        }else if(imc>18 && imc <=24.9){
+            document.write(" Seu imc está normal!!");
+        }else if(imc>25 && imc <=28.9){
+            document.write(" Seu imc indica está em sobrepeso!!");
+        }
+        }
+    
+
+
